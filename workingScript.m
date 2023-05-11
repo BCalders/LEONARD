c29117ecbde7867d6eabd483f25b293bbcca3092
bas <bas.calders@student.uantwerpen.be>
2023-05-11 11:10:59 +0200

    updated with Rreze and added globalstar

--- workingScript.m
+++ workingScript.m
@@ -21,33 +21,30 @@ format long
 
 disp("Setting up...")
 
-simTime = 10;
+simTime = 60;
 % startTime = datetime("5-july-2022 13:17");
 % startTime = datetime("11-september-2022 17:53");
-startTime = datetime("7-march-2023 04:22");
-% startTime = datetime("16-april-2023 16:05:33");
-stopTime = startTime + minutes(simTime);
-sampleTime = 60;
-samplesPerMinute = sampleTime/60;
+startTime = datetime("7-march-2023 04:22:00");
+% startTime = datetime("16-april-2023 16:40:30");
+stopTime = startTime + seconds(simTime);
+sampleTime = 1;
 
 c = physconst("Lightspeed");
 
 sc = satelliteScenario(startTime, stopTime, sampleTime);
 
-gs = groundStation(sc, 51.17800903509613, 4.418814450257098, 'Name', "CGB - Receiver");
+% gs = groundStation(sc, 51.17800903509613, 4.418814450257098, 'Name', "CGB - Receiver");
 % gs = groundStation(sc, 0.5108574230657834, 33.13331679803374, 'Name', "Uganda - Reciever");
-% gs = groundStation(sc, -12.051334463667322, -77.012949622246, 'Name', "Peru - Reciever");
+gs = groundStation(sc, -12.051334463667322, -77.012949622246, 'Name', "Peru - Reciever");
 gsEcefPos = lla2ecef([gs.Latitude, gs.Longitude, gs.Altitude])';
 
-SAT.all = satellite(sc, "tle/iridium.tle");     % Iridium satellites used as a testing satellite set with global coverage
+% globalStar
+SAT.all = satellite(sc, "tle/globalstar.tle");     % Iridium satellites used as a testing satellite set with global coverage
 numSats = length(SAT.all);
 SAT.femit = 1610e6;        % Avg emitted frequency in Hz used by Iridium
-initState = [4.6e+06, 1e+06, 4.2e+06, 0, 0];  % init pos in Rome for added difficulty
-% initState = [4.12e+06, -4.55e+06, -1.72e+06, 0, 0];  % init pos in Brasilia for added difficulty
+% initState = [4.6e+06, 1e+06, 4.2e+06, 2.99, 100];  % init pos in Rome for added difficulty
+initState = [4.12e+06, -4.55e+06, -1.72e+06, 0, 0];  % init pos in Brasilia for added difficulty
 %[4e+06, 3e+05, 5e+06, 0, 0]; init pos in the netherlands
-
-breakLoop = false;
-loopBroken = false;
 
 disp("Setup complete")
 
@@ -58,28 +55,39 @@ hold on
 llaStates = ecef2lla(initState(1, 1:3));
 geoscatter(llaStates(1), llaStates(2), 'filled', 'MarkerFaceColor','c')
 
+% define access for all timepoints
+ac = access(SAT.all, gs);
+acStatus = accessStatus(ac);
+
 %% Calculation
 
 disp("Starting calculation...")
 
-% define access for all timepoints
-ac = access(SAT.all, gs);
-acStatus = accessStatus(ac);
 
 estimatedState = initState;
 
 for currTime = 1:simTime
     focussedSat = 0;                                % know which satellite in view is being focussed on
+    satsInView = nnz(acStatus(: ,currTime));
+
+    if satsInView < 3
+        disp("Not Enough satellites to perform localization")
+        break;
+    end
+
+    deltaD = zeros(satsInView, 1);
+    H = zeros(satsInView, 5);
+
     for currSat = 1:numSats
         if acStatus(currSat, currTime) == 1         % only calculate if satellite is in view
 
             focussedSat = focussedSat + 1;          % indicate that next satellite is being focussed on
 
             % determine satellite position and velocity
-            [satPos,satVel] = states(SAT.all(currSat), startTime + minutes(currTime), "Coordinateframe", "ecef"); % using currtime and not currtime - 1 to ensure a previous time is present in the SC 
+            [satPos,satVel] = states(SAT.all(currSat), startTime + seconds(currTime), "Coordinateframe", "ecef"); % using currtime and not currtime - 1 to ensure a previous time is present in the SC 
 
             % setting previous velocity
-            [satPosPrev,satVelPrev] = states(SAT.all(currSat), startTime + minutes(currTime - 1), "Coordinateframe", "ecef");
+            [satPosPrev,satVelPrev] = states(SAT.all(currSat), startTime + seconds(currTime - 1), "Coordinateframe", "ecef");
             sat2gsVelPrev = calcRelVel(satPosPrev, satVelPrev, gsEcefPos); 
 
             % calculate relative velocity
@@ -98,48 +106,23 @@ for currTime = 1:simTime
             unitVector = rangeVect / rho;
             
             rhoDot = dot(satVel, unitVector);               % Range Rate (m/s)
-%             rhoDotDot = satAcc;
             rhoDotDot = dot(satAcc, unitVector);            % Range Acceleration (m/s^2)
             
             eDot = (1/rho) * (satVel - unitVector * rhoDot);
 
-            if focussedSat > 1     % if it is the first timepoint, there is no H yet for this timepoint
-                H = [H ; eDot', 1, -rhoDotDot];
-            else 
-                H = [eDot', 1, -rhoDotDot];
-            end 
+            H(focussedSat,:) = [eDot', 1, -rhoDotDot];
             
-            D_predicted = speed2Dop(SAT.femit, rhoDot);       % predicted Doppler shift (Hz));
-            deltaDoppler = fobs - D_predicted;           % deltaDoppler (Hz)
-            deltaD = dop2Speed(SAT.femit, deltaDoppler) + c;      % deltaD (m/s)
-
-%             if deltaD == -C                             % break loops if accuracy can not become any better
-%                 disp("maximum achievable accuracy is highly likely") % TODO -> move this to time loop -> deltaDMatrix should be all C for loopbreak
-%                 if breakLoop
-%                     loopBroken = true;
-%                 end
-%                 breakLoop = true;
-%             end
-
-            if focussedSat > 1     % if it is the first satellite, there is no deltaDMatrix yet for this timepoint
-                deltaDMatrix = [deltaDMatrix; deltaD];
-            else
-                deltaDMatrix = deltaD;
-            end
+            D_predicted = rhoDot - estimatedState(4);   % predicted D (Hz));
+            deltaD(focussedSat) = sat2gsVel - D_predicted;
         end
     end
-    deltaX = H \ deltaDMatrix;                                % use of backslash operator to invert H
+    deltaX = H \ deltaD;                                % use of backslash operator to invert H
     
-    estimatedState = estimatedState + deltaX';
+    estimatedState = estimatedState - deltaX';
     results(currTime,:) = estimatedState;
     llaStates(currTime, :) = ecef2lla(estimatedState(1:3));
     % drawnow;
     disp(currTime + ": Current position: x: " + estimatedState(1) + " y: " + estimatedState(2) + " z: " + estimatedState(3))
-    
-    if loopBroken               % break loop when accuracy is perfect -> save calculations
-        disp("maximum achievable accuracy reached")
-        break;
-    end
 end
 disp("Calculation complete")
 
@@ -147,12 +130,12 @@ disp("plotting...")
 
 names = string(1:simTime);  
 geoscatter(llaStates(:, 1), llaStates(:, 2), 'b')
-text(llaStates(:,1),llaStates(:,2),names)
+% text(llaStates(:,1),llaStates(:,2),names)
 figure;
 geoscatter(gs.Latitude, gs.Longitude, 'filled', 'MarkerFaceColor', 'r')
 hold on
 geoscatter(llaStates(:, 1), llaStates(:, 2), 'xb')
-text(llaStates(:,1),llaStates(:,2),names)
+% text(llaStates(:,1),llaStates(:,2),names)
 title("Position Estimations zoom")
 geolimits([gs.Latitude - 0.001,  gs.Latitude + 0.001], [gs.Longitude - 0.02 gs.Longitude + 0.02])
 % geobasemap('streets')
@@ -181,7 +164,7 @@ end
 
 function [relativeVelocity] = dop2Speed(fEmit, fObserved)
     c = physconst("Lightspeed");
-    relativeVelocity = c * (fObserved - fEmit) / fEmit;
+    relativeVelocity = c * (fObserved) / fEmit;
 end
 
 function [relativeVelocity] = calcRelVel(satPos, satVel, recPos)
@@ -223,3 +206,13 @@ end
 % - first implementation done
 % -
 % -
+
+% 15/5
+% moving ue
+% beschrijven van alles, sats hoogte, freq en alles algos dopp -> dan resultaten
+% die hebben ook grafieken alle getallen. discussie is waarom uitschrijven
+% resutaten en discussie in puntjes
+% intro sota en conclusie kan gelijkaardig op vorige
+% 23/5 
+% moving ue zeker klaar
+% eerste uitgeschreven versie doorsturen
